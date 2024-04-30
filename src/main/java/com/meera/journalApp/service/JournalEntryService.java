9e8b613fd33453bae9d0f6827991b2e38b74b29d
dev-bhavya <rk.bhavyamittal@gmail.com>
2024-04-30 11:38:32 +0530

    Added post construct method & AppCache Feature

--- src/main/java/com/meera/journalApp/service/JournalEntryService.java
+++ src/main/java/com/meera/journalApp/service/JournalEntryService.java
@@ -7,14 +7,14 @@ import com.meera.journalApp.repository.UserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Service
 @Slf4j
 public class JournalEntryService {
 
